fc4081629c9c2015193dd061e37bc42297962206
stressfuluser <stressfuluser@proton.me>
2025-11-24 21:46:29 +0800

    Linting css

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
+  },
+]);
